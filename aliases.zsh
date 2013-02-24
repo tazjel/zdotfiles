33d3ffb0386c1ddb7e8cdc32d532b1ab3f27cc65
tazjel <tazjel@gmail.com>
2013-02-24 04:19:49 +0300

    Project:tazjel

---
+++ aliases.zsh
@@ -0,0 +1,15 @@
+
+#"   01. General ................. General Vim behavior                       "
+#"   02. Events .................. General autocmd events                     "
+#"   03. Theme/Colors ............ Colors, fonts, etc.                        "
+#"   04. Vim UI .................. User interface behavior                    "
+#"   05. Text Formatting/Layout .. Text, tab, indentation related             "
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#" 01. General                                                                "
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""l
+#" MAKE IT EASY TO UPDATE/RELOAD _vimrc
+##:nmap ,s :source $VIM/_vimrc
+##:nmap ,v :e $VIM/_vimrc
+##:e $MYVIMRC         : edits your _vimrc whereever it might be  [N]
+#--------------
