6e1d3b7475f6614cf067e567cb70722170fefcb5
abobani <ubuntu.ubuntu-domain>
2013-05-17 07:06:15 +0300

    Setup zdotfiles to work with both windows and kubuntu

--- 1/.zsh/al.zsh
+++ 1/.zsh/al.zsh
@@ -1,94 +1,55 @@
-
-alias wbig='find . -type f -exec du -sh {} + | sort -hr | head'
+##
+##
 #
-# Put a console clock in top right corner
-#while true; do echo -ne "\e[s\e[0;$((COLUMNS-27))H$(date)\e[u"; sleep 1; done &
-# Symlink all files from a base directory to a target directory
-#for f in $(ls -d /base/*); do ln -s $f /target; done && ls -al /target
-#
-alias 'wzsh_history'='tail -f .zsh_history | while read line; do echo -n $(date -u -Ins); echo -e "\t$line"; done'
-
-function wwiki () {
-./zdotfiles/Tools/wikisole.sh $1
-}
-
-#mkdir -p /home/bani/.zdotfiles/vimbackup/
-function wwgg (){
-    curl -u tazjel --silent "https://mail.google.com/mail/feed/atom" | awk 'BEGIN{FS="\n";RS="(</entry>\n)?<entry>"}NR!=1{print "\033[1;31m"$9"\033[0;32m ("$10")\033[0m:\t\033[1;33m"$2"\033[0m"}' | sed -e 's,<[^>]*>,,g' | column -t -s $'\t'
-}
-
-
-
-#function wgmail () {
-#curl -u tazjel --silent "https://mail.google.com/mail/feed/atom" | awk 'BEGIN{FS="\n";RS="(</entry>\n)?<entry>"}NR!=1{print "\033[1;31m"$9"\033[0;32m ("$10")\033[0m:\t\033[1;33m"$2"\033[0m"}' | sed -e 's,<[^>]*>,,g' | column -t -s $'\t''
-#}
-
-function wpr {
-grep '^'$(date +\%d) $HOME/zdotfiles/Time/$(date +\%m) | awk '{print " \n\n Fajer   : " $2 "\n SHAROOK : " $3 "\n Dohr    : " $4  "\n ASER    : " $5  "\n MAGREP  : " $6  "\n ISHA    : " $7 }'
-}
-
+########################################################
 # zdotfiles aliases
-alias 'wtoday'='grep "^"$(date +\%d) $HOME/zdotfiles/Time/april_prayertime'
-# tail -n3 .zsh_history | sort -u | sed 's/^.*\;//g' >> zdotfiles/6/wfindw ; tail zdotfiles/6/wfindw
-# find /var/cache/apt -not -mtime -7
-alias wbg='ls -Sh **/*(.Lm+30) | tail'
-alias wlv='find ~ -maxdepth 1 -mindepth 1 -name   \*vim\* -type f'
-alias wlz='find ~ -maxdepth 1 -mindepth 1 -name   \*zsh\* -type f'
-alias wd='du -h --time --max-depth=1 | sort -hr #######################################################'
-#wip | grep S | cut -d' ' -f 6
-alias wipsj='nmap -sP 192.168.1.0/24 | grep "Nmap scan report for" | grep S | cut -d" " -f 6 '
-alias wip_host='nmap -sP 192.168.1.0/24 | grep "Nmap scan report for"| cut -d' ' -f 5'
-alias wips='nmap -sP 192.168.1.0/24 | grep "Nmap scan report for"| cut -d' ' -f 6'
-alias wip='nmap -sP 192.168.1.0/24 | grep "Nmap scan report for" '
-alias wff='diff -qr "$1" "$2" '
-#diff -u <(grep -vE '^(#|$)' file1) <(grep -vE '^(#|$)' file2)
-function cmdfu(){ local t=~/cmdfu;echo -e "\n# $1 {{{1">>$t;curl -L "commandlinefu.com/commands/matching/$1/`echo -n $1|base64`/plaintext"|sed '1,2d;s/^#.*/& {{{2/g'>$t;vim -u /dev/null -c "set ft=sh fdm=marker fdl=1 noswf" -M $t;rm $t; }
-alias wd='cal ; date +%c'
-alias 'whh'='grep "$1" ~/.zsh_history'
-alias ba='less ~/.bash_aliases'
-alias apt="sudo apt-get update && sudo apt-get upgrade && sudo apt-get clean"
-
-alias 'Ww_dmesg'='sudo dmesg | tail -4'
-#Q="YOURSEARCH"; GOOG_URL="http://www.google.com/search?q="; AGENT="Mozilla/4.0"; stream=$(curl -A "$AGENT" -skLm 10 "${GOOG_URL}\"${Q/\ /+}\"" | grep -oP '\/url\?q=.+?&amp' | sed 's/\/url?q=//;s/&amp//'); echo -e "${stream//\%/\x}"
-
-function google { Q="$@"; GOOG_URL='https://www.google.de/search?tbs=li:1&q='; AGENT="Mozilla/4.0"; stream=$(curl -A "$AGENT" -skLm 10 "${GOOG_URL}${Q//\ /+}" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||'); echo -e "${stream//\%/\x}"; }
-#for i in $(ls ~/script); do alias s_$i='sh ~/script/$i';done;
-#
-#1
-#
-#alias confcat="sed -e 's/[#;].*//;/^\s*$/d' "$@""
+########################################################
 
 alf=$HOME/.oh-my-zsh/lib/aliases.zsh
 
 alias wal='cat $alf | grep'
 alias al='alias | grep'
-alias zz='vim $HOME/zdotfiles/1/.zsh/al.zsh && source $HOME/zdotfiles/1/.zsh/al.zsh'
+#www wbani.unsettings |  sed '/youtube/d'
+#ag -i $1 ./abobani/clf-ALL.txt | sort -n | uniq --ignore-case >> zwgrep.txt
+alias zz='vim $HOME/.oh-my-zsh/lib/aliases.zsh && source $HOME/.oh-my-zsh/lib/aliases.zsh'
 
-alias zzzz=' vim $HOME/.zshrc && source $HOME/.zshrc'
+alias zzzz=' vim $HOME/.zshrc'
+alias zzzzz='source $HOME/.zshrc'
 
+
+function walw {
+#USB storage
+rsync -avz --progress /home/bani/.oh-my-zsh/lib/aliases.zsh /media/Transcend/tools/w/vimrc.local
+rsync -avz --progress /home/bani/.oh-my-zsh/lib/aliases.zsh /media/Transcend/tools/w/aliases.zsh
+#repo
+rsync -avz --progress /home/bani/.vimrc.local $HOME/zdotfiles/Tools/2/vimrc.local
+rsync -avz --progress /home/bani/.oh-my-zsh/lib/aliases.zsh $HOME/zdotfiles/Tools/1/aliases.zsh
+cd $HOME/zdotfiles && aa
+alw;
+}
 alias lh='ls -a | egrep "^\."'
-alias lhd='ls -A | egrep "^\." '
+alias lhd='ls -A | egrep "^\."'
 #
 #####################################
-#alias 'wWcll'='clear;pwd;ls;date -R;echo $PWD'
+alias 'wWcll'='clear;pwd;ls;date -R;echo $PWD'
 
 #################################
-#  rsync
+#  rsync 
 #
 
-#rsync mobile
+#rsync mobile 
 alias wzi='rsync -e ssh -r --progress root@zphone.local:/var/mobile/qq.sh ~/qq.sh'
 
 ##alias 'wadd'='rsync -avz --progress -e  "ssh -p 2233" root@192.168.1.5:/var/mobile/AddressBook.sqlitedb ~/'
 ###############################
 alias 'zrf1'="rsync -avz --progress '$1' '/media/Transcend/w/$1'"
 function zbp {
-for xx in ~/.* ;do
-    rsync -avz --progress $HOME/.* $HOME/zrsync
+for xx in ~/.* ;do 
+    rsync -avz --progress /home/bani/.* /media/Transcend/bani/R/
     echo $xx
 done;}
 
-#sudo showkey -k
+#
 alias xxx="sudo sixpair;sudo sixad --stop;sudo sixad --start"
 alias __="sudo"
 
@@ -99,7 +60,7 @@ alias 'cc'='vim /home/bani/zdotfiles/Tools/1/aliasw.sh'
 
 alias 'wWw'='vim /home/bani/.config/updot/updot.conf'
 alias 'wW'='pwd'
-a``lias 'w'='ls -a;echo $pwd;date'
+alias 'w'='ls -a; ls'
 alias 'ww'='vim'
 alias 'www'='cat'
 #alias 'wwwww'='sudo su'
@@ -141,15 +102,15 @@ alias ..5='cd ../../../../../'
 ########## zdotfiles
 #
 function WW {
-    desc="ADD DESCRIPTION..."
-    if [ -n "$3" ]; then
-        desc="$3"
-    fi
-    echo "" >> $HOME/zdotfiles/.oh-my-zsh/lib/waliases.zsh
-    echo "###" >> $HOME/zdotfiles/.oh-my-zsh/lib/waliases.zsh
-    echo "alias '$1'='$2'  " >> $HOME/zdotfiles/.oh-my-zsh/lib/aliases.zsh
-    source $HOME/zdotfiles/.oh-my-zsh/lib/aliases.zsh
-    echo "" && echo "- Alias added  $1 = $2 " && echo "";
+desc="ADD DESCRIPTION..."
+if [ -n "$3" ]; then
+desc="$3"
+fi
+echo "" >> /home/bani/.oh-my-zsh/lib/aliases.zsh
+echo "###" >> /home/bani/.oh-my-zsh/lib/aliases.zsh
+echo "alias '$1'='$2'" >> /home/bani/.oh-my-zsh/lib/aliases.zsh
+source /home/bani/.oh-my-zsh/lib/aliases.zsh
+echo "" && echo "- Alias added  $1 = $2 " && echo "";
 }
 
 
@@ -164,10 +125,10 @@ echo '/home/bani/zdotfiles/Tools/1/aliasw.sh'
 
 ##########
 function glll {
-    cd $HOME/zdotfiles;
-    git add -A
-    git commit -a
-    git push origin master;
+cd /home/bani/zdotfiles
+git add -A
+git commit -a
+git push origin master;
 }
 ##########
 #
@@ -181,7 +142,6 @@ function glll {
 
 alias wcr='wc `find . -type f`'
 alias wget='wget --content-disposition'
-alias whead='curl --head $1'
 alias whead='curl --head $1'
 alias which-command=whence
 alias wotgobblemem='ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -15'
@@ -209,7 +169,7 @@ alias 7='cd +7'
 alias 8='cd +8'
 alias 9='cd +9'
 alias ANYNAMEHERE=' ssh YOURWEBSITE.com -l USERNAME -p PORTNUMBERHERE'
-alias W='echo'
+#alias W=cat
 alias a2-restart='sudo service apache2 restart'
 alias a2r='/etc/init.d/apache2 restart'
 alias aa='git add -A .'
@@ -382,6 +342,7 @@ alias realias='$EDITOR ~/.bash_aliases && source ~/.bash_aliases'
 alias refunction='$EDITOR ~/.bash_functions && source ~/.bash_functions'
 alias reload='source $HOME/.bashrc 1>/dev/null'
 alias remove='sudo apt-get remove'
+alias rename=mv
 alias rfresh=' rsync -avz --progress -e  "ssh -p 2233" user@110.11.11.11:/your/remote/path /my/local/path'
 alias rm='rm -I'
 alias sa='ssh-add '
@@ -480,15 +441,15 @@ alias 'wmkdir'='mkdir -p /home/bani/doc/bar && cd $_'
     #cp $1 `basename $1`-`date +%Y%m%d%H%M`.backup ;
 #}
 #ooo
-
-#WWW(){
-        #echo -n "Comment> "
-        #read comment
-        #name="$1"
-        #shift
-        #echo "alias $name=\"$*\" $comment" >> $HOME/.zshenv"
-        #alias $name="$*"
-#}
+#
+WWW(){
+        echo -n "Comment> "
+        read comment
+        name="$1"
+        shift
+        echo "alias $name=\"$*\"        # $comment" >> $HOME/.zshenv
+        alias $name="$*"
+}
 
 
 #hack()
@@ -509,62 +470,35 @@ function wW_grep_logs(){
 
 ###
 
-alias 00=$HOME/zdotfiles
-alias 11=$HOME/zdotfiles/1
-alias 22=$HOME/zdotfiles/2
+alias 00=/home/bani/zdotfiles
 
-alias 33$HOME/zdotfiles/3
-alias 44$HOME/zdotfiles/4
-alias 55$HOME/zdotfiles/5
-alias 66$HOME/zdotfiles/6
 alias 'wW_sudo_su'='sudo su'
-        alias 'wW_grep_1_2'='grep -GRh $1 ~ | sort | uniq -c | sort -nr >> $2 && tail  $2'
-alias 'wW_grep_1'='grep $1 ~/.zsh_history | sort | uniq -c | sort -nr'
+alias 'wW_grep_1'='grep -GRh $1 ~ | sort | uniq -c | sort -nr >> $2 && tail  $2'
 alias -g 'wW___help'='--help'
 alias -g Gh='--help | grep'
 alias -g G='| grep'
 
 ###
-alias 'wW__for_in1_do_2'='for i in $1;do $2;done;'
+alias 'wW__for_in1_do_2'='for i in $1;do;$2;done;'
 #ls -lF /etc; pwd;who;ps
 #
 ##!/bin/bash
 function ww10 {
-for i in $(seq 0 10);do
+for i in $(seq 1 10);do
     echo $i
 done
 }
+function ww_diff {
+find . -type f -name '*.csv' -exec sh -c '
+  file="$0"
+  echo "$file"
+  diff "$file" "/some/other/path/$file"
+  read line </dev/tty
+' {} ';'
+}
 
 
-#function ww_diff {
-#find . -type f -name '*.csv' -exec sh -c '
-  ##file="$0"
-  ##echo "$file"
-  ##diff "$file" "/some/other/path/$file"
-  ##read line </dev/tty
-##' {} ';'
-##}
+function alg {
+grep $1 $HOME/.zsh_history | sed 's/^.*;//g'
 
-
-#function alg {
-#grep $1 $HOME/.zsh_history | sed 's/^.*;//g'
-
-#}
-
-#alias wwww_ksystemlog='/usr/bin/ksystemlog --icon utilities-log-viewer -caption KSystemLog'
-#ft() {
-
-#find . -name "$2" -exec grep -il "$1" {} \;
-#}
-
-#alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'''
-#alias l.='ls -d .* --color=auto'
-#alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
-#alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
-#alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
-#alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
-
-####
-alias 'wW_aptitude'='sudo aptitude -f'
-alias '_r'='sudo reboot'  
-#while read line; do echo "$(date),$(hostname),$line"; done < somefile.txt
+}
