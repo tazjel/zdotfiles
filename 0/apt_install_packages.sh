1af10842f3e20e1d328f42aeca7504dc01005ad7
Ahmed Bani <tazjel@gmail.com>
2013-05-30 19:48:27 +0300

    Setup the Tools

--- 0/apt_install_packages.sh
+++ 0/apt_install_packages.sh
@@ -1,19 +1,30 @@
-aptitude 
+
+#!/bin/bash
+### list of packages to install.
+
+aptitude
 zsh
 git
 vim vim-scripts vim-doc vim-latexsuite vim-gui-common vim-gnome
+autotools-dev
+automake
+build-essential
+python-dev
+automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
+
+                            ################
+
 git clone https://github.com/ggreer/the_silver_searcher
 the_silver_searcher && ./build.sh && install
  ./build.sh && install
-sudo apt-get install autotools-dev
-sudo apt-get install automake
+
+
+
+                            ################
+#check MACHINE_TYPE
 MACHINE_TYPE=`uname -m`
    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
    # 64-bit stuff here
    else
    # 32-bit stuff here
-   fi" >> ~/zdotfiles/1/get_machine_type_64_32
-
-sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-sudo apt-get install build-essential python-dev
+   fi
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-﻿dotfiles
+﻿Dotfiles
 =========
 Work in progress
 
