9516f4f6cc35c3c7ddc36ccc90c420ed0a62964a
Ahmed Bani <tazjel@gmail.com>
2013-08-31 13:59:10 +0300

    Update getw2p.sh

--- 9/web2py/getw2p.sh
+++ 9/web2py/getw2p.sh
@@ -1,5 +1,3 @@
-
 git clone git://github.com/web2py/web2py.git
-cd web2py/applications
-
-hg clone https://bitbucket.org/speedbird/qastack /home/bani/zdotfiles/Tools/9/web2py/applications/qastack
+cd ~/zdotfiles/9/web2py/applications
+hg clone https://bitbucket.org/speedbird/qastack
