7d05ad5e81a4d443514b27a94771a76e4bbca84e
Ahmed Bani <tazjel@gmail.com>
2013-08-31 14:04:31 +0300

    Update git_w2p_qastack.sh

--- 9/web2py/git_w2p_qastack.sh
+++ 9/web2py/git_w2p_qastack.sh
@@ -1,3 +1,17 @@
-git clone git://github.com/web2py/web2py.git
+
+wget http://www.web2py.com/examples/static/web2py_src.zip
+unzip web2py_src.zip
+
+# git clone git://github.com/web2py/web2py.git
 cd web2py/applications
 hg clone https://bitbucket.org/speedbird/qastack
+
+python -c "from gluon.main import save_password; save_password(raw_input('admin  password: '),433)"
+
+
+#Edit the "/var/www/<username>_pythonanywhere_com_wsgi.py" file and write in it:
+
+#import sys
+#path = '/home/<username>/web2py'
+#if path not in sys.path: sys.path.append(path)
+#from wsgihandler import application # the web2py handler
