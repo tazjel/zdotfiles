6296975c7f0d28261f92fdffe6ea346aff336324
Ahmed Bani <tazjel@gmail.com>
2013-08-31 13:57:03 +0300

    w

--- 9/web2py/getw2p.sh
+++ 9/web2py/getw2p.sh
@@ -1,4 +1,5 @@
-git clone git://github.com/web2py/web2py.git
 
+git clone git://github.com/web2py/web2py.git
+cd web2py/applications
 
 hg clone https://bitbucket.org/speedbird/qastack /home/bani/zdotfiles/Tools/9/web2py/applications/qastack
