9637ea33d7855f82072751b29c32cacc26524ab3
Ahmed Bani <tazjel@gmail.com>
2013-09-06 16:59:33 +0300

    Prepare for Conflict Analysis & Resolutions System Fixed Autokey at the 6th

---
+++ blender.py
@@ -0,0 +1,42 @@
+i" blender
+iabb <buffer> BB
+\import bpy<cr>
+\from mathutils import *<cr>
+\from math import *<cr>
+\from bpy.props import *<cr>
+\import os<cr>
+\import time
+\<esc>:cal S(1)<cr>mport bpy
+from mathutils import *
+from math import *
+from bpy.props import *
+import os
+import time
+
+# Init
+position_vector = []
+
+# Open file
+file = open("~/zdotfiles/where.csv", "r")
+
+# Loop over line in file
+for line in file:
+
+    # Split line at ";"
+    splittet_line = line.split(";")
+
+    # Append new postion
+    position_vector.append(
+        Vector((float(splittet_line[0]),
+                float(splittet_line[1]),
+                float(splittet_line[2]))))
+
+# Close file
+file.close()
+
+# Get first selected object
+selected_object = bpy.context.selected_objects[0]
+
+# Get first selected object
+for position in position_vector:
+    selected_object.location = position
--- check_install.sh.1
+++
@@ -1,8 +0,0 @@
-
-#check MACHINE_TYPE
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == "x86_64"] ; then
-    echo "64-bit stuff here"
-else
-    echo "32-bit stuff here"
-fi
