664ac615f02a450483399563cb44299ca671e78e
Ahmed Bani <tazjel@gmail.com>
2013-10-09 13:44:31 +0300

    Focuse on python

--- 0/Inbox_installers/1_setup_git.sh
+++ 0/Inbox_installers/1_setup_git.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-git_config_global ()
-{
-    git config --global user.name 'Ahmed Al-Ghamdi'
-    git config --global user.email 'tazjel@gmail.com'
-    git config --list
-    git config --global color.ui auto
-    git config --global credential.helper cache
-    git config --global credential.helper 'cache --timeout=3600'
-}
-
 ssh-keygen_rsa ()
 {
     ssh-keygen -t rsa -C 'tazjel@gmail.com'
