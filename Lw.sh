bd4744f1071637476f8ee3938f2d9ee19d5179bf
tazjel <tazjel@gmail.com>
2014-01-13 23:18:22 +0300

    update

--- Lw.sh
+++ Lw.sh
@@ -1,9 +1,9 @@
 bkup_z() {
-unset f
-for f in "$T_FILES"; do 
-source="${LINKS}/${f}" && \
-echo "Source = $source" \ 
-target="${HOME}/$f" && echo "Target = $target/$f"; done\
-unset f
+    unset f
+    for f in "$T_FILES"; do 
+    source="${LINKS}/${f}" && \
+    echo "Source = $source" \ 
+    target="${HOME}/$f" && echo "Target = $target/$f"; done\
+    unset f
 }
 
