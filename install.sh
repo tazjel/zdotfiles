6acaa73e38cc06602c1324f3db1b57482c5869c4
Ahmed Bani <tazjel@gmail.com>
2014-04-02 16:29:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #! /usr/bin/env sh
 
+ZD="~/zdotfiles"
+
 zapt_wheezy ()
 {
      apt-get update && apt-get upgrade
 apt-get install zsh git curl ranger python 
 }
 
+cd $HOME && git clone 
+
+https://github.com/tazjel/zdotfiles.git
+
+rm ~/.zshrc; ln -s ~/zdotfiles/link/.zshrc
+rm ~/.vimrc.local
+
+if [ -f  ]
+
+mkdir -p 
+ln -s ~/zdotfiles/link/.vimrc.local zsh && ranger
 
 
