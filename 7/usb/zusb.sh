aadb3caf81bcf9cc74edb22208ddc086c345fe2d
Ahmed Bani <tazjel@gmail.com>
2014-05-23 08:35:56 +0300

    Update zusb.sh

--- 7/usb/zusb.sh
+++ 7/usb/zusb.sh
@@ -2,9 +2,15 @@
 
 # Find usb devices
 
-usb_devices() {
+usb_devices_product() {
     cat /sys/bus/usb/devices/usb*/product
-    }
-  
-       
-       
+}
+
+
+lsusb_camera() {
+    lsusb -v | grep Camera
+}
+
+usb_devices_show() {
+    usb-devices
+}
