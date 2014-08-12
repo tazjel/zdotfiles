59b69b4a6979e81f027da2703349d5f4f09d0d3b
Ahmed Bani <tazjel@gmail.com>
2014-08-12 20:23:49 +0300

    Create pyport.py

---
+++ 7/serial/pyport.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import serial ## Load the serial library
+
+## Select and configure the port
+myPort = serial.Serial('/dev/ttyS1', 115200, timeout = 10)
+
+## Dump some data out of the port
+myPort.write("Hello, world!")
+
+## Wait for data to come in- one byte, only
+x = myPort.read()
+
+## Echo the data to the command prompt
+print "You entered " + x
