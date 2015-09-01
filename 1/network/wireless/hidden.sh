60c966de43d1287e54575d06bdb16683dfeb3525
Ahmed Bani <tazjel@gmail.com>
2015-09-01 21:43:31 +0300

    Create hidden.sh

---
+++ 1/network/wireless/hidden.sh
@@ -0,0 +1,7 @@
+# Problem : Connecting to a hidden wireless network on Kubuntu
+# On Dell Inspiron : change zBani!
+
+sudo iwlist wlan0 scan essid zBani
+
+#Reference :
+# http://superuser.com/questions/118551/connecting-to-a-hidden-wireless-network-on-kubuntu
