7c7a82bc42e636803c691ef092f63a91af341769
Ahmed Bani <tazjel@gmail.com>
2016-01-13 08:04:18 +0300

    Create install_on_ubuntu.sh

---
+++ 8/0/android/install_on_ubuntu.sh
@@ -0,0 +1,27 @@
+# 
+    
+sudo apt-get install unzip
+sudo tar xvzf android-studio-ide-135.1641136-linux.zip
+cd android-studio-ide-135.1641136-linux
+./studio.sh
+
+
+sudo apt-get install unzip
+sudo tar xvzf android-studio-ide-135.1641136-linux.zip
+cd android-studio-ide-135.1641136-linux
+./studio.sh
+
+# JDK 1.7 is required for Studio 1.0 onwards:
+
+# Download the ubuntu zip from the d.android.com and repeat the steps from above
+# Download the jdk 1.7 by executing the following commands in terminal as mentioned webupd8.
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer
+
+# Open Android Studio and install the SDK tools.
+
+
+
+# Reference : http://stackoverflow.com/questions/28314139/how-to-install-android-studio-in-ubuntu
