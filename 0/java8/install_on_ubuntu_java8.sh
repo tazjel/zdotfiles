c303a6b38cd8113f8c50d054c277bf92ed124a70
Ahmed Bani <tazjel@gmail.com>
2016-04-13 16:02:07 +0300

    Create install_on_ubuntu_java8.sh

---
+++ 0/java8/install_on_ubuntu_java8.sh
@@ -0,0 +1,8 @@
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+java -version
+sudo apt-get install oracle-java8-set-default
+sudo update-java-alternatives -s java-8-oracle
