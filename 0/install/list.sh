d4a3bcfb176a10f143cb4960f7eea49ab2aa163e
Ahmed Bani <tazjel@gmail.com>
2016-07-10 17:59:09 +0300

    Clean the folders

--- 0/install/list.sh
+++
@@ -1,5 +0,0 @@
-
-sudo apt-get install fontforge                                                                                  
-sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev mercurial libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
-
-
