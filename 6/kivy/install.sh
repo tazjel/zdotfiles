7f01c91fbbc8a47628c6dae64502cd0ea9e8d820
Ahmed Bani <tazjel@gmail.com>
2016-07-20 02:31:51 +0300

    update

---
+++ 6/kivy/install.sh
@@ -0,0 +1,3 @@
+git clone http://github.com/kivy/kivy-designer/
+cd kivy-designer
+python main.py
---
+++ 6/speech/install_audio_analysis.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+pyAudioAnalysis = "https://github.com/tyiannak/pyAudioAnalysi://github.com/tyiannak/pyAudioAnalysis.git"
+
+
+def z_clone_github_repo(gitz):
+    return dict()
