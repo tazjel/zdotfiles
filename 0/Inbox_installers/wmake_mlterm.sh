330e41df6b672cb319c4bf9af1bd98015562031d
Ahmed Bani <tazjel@gmail.com>
2016-11-01 16:27:55 +0300

    Delete wmake_mlterm.sh

--- 0/Inbox_installers/wmake_mlterm.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/bash
-
-
-
-./configure --enable-fribidi --enable-deb`  ug --enable-optimize-redrawing --with-tools --enable-uim --enable-m17nlib --enable-scim --enable-ibus --enable-pty-helper --enable-vt52 && make && make install
