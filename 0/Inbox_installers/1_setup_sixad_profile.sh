79884bc5248fb4d48d3394ac8acfcced2873db62
Ahmed Bani <tazjel@gmail.com>
2016-11-01 11:36:48 +0300

    Delete 1_setup_sixad_profile.sh

--- 0/Inbox_installers/1_setup_sixad_profile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#/usr/share/qtsixa/profiles
-#/etc/sixad
-
-cp ~/zdotfiles/6/sixad/tazjel_keys_profile /var/lib/sixad/profiles/34:C7:31:35:08:44
-cp ~/zdotfiles/6/sixad/tazjel_keys_profile /var/lib/sixad/profiles/default
