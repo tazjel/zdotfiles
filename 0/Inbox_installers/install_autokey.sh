1ac5f801cab2734b2c7e5d95fd893c4fd28e3750
Ahmed Bani <tazjel@gmail.com>
2016-11-01 16:11:51 +0300

    Delete install_autokey.sh

--- 0/Inbox_installers/install_autokey.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get remove autokey
-sudo add-apt-repository ppa:cdekter/ppa
-sudo apt-get update
-sudo apt-get install autokey-gtk
-cd ~/.config
-mv autokey autokey-backup
-ln -s $HOME/zdotfiles/6/autokey
