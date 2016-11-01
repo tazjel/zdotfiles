a59d1f4775a7d8f8e53dcb35020e2faf9c21418b
Ahmed Bani <tazjel@gmail.com>
2016-11-01 11:37:35 +0300

    Delete andriod-install.sh

--- 0/Inbox_installers/andriod-install.sh
+++
@@ -1,9 +0,0 @@
-Ref: http://www.liberiangeek.net/2013/02/install-android-sdk-adt-bundle-in-ubuntu-12-10-quantal-quetzal/
-cd ~
-wget http://dl.google.com/android/android-sdk_r21.1-linux.tgz
-tar -xvzf android-sdk_r21.1-linux.tgz
-cd android-sdk-linux/tools/
-./android
-gedit ~/.bashrc
-#
-export PATH=${PATH}:~/android-sdk-linux/platform-tools
