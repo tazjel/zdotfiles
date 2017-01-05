d355a379fa217387b049b88c6fac52c3bebc2374
Ahmed Bani <tazjel@gmail.com>
2017-01-05 14:00:33 +0300

    Create python-rpy2.sh

---
+++ 0/install/python/python-rpy2.sh
@@ -0,0 +1,5 @@
+# Install and configure Rpy2 on opensuse
+# If you use Ubuntu's package management, it should pull in all the dependencies, including R:
+# Ref: http://stackoverflow.com/questions/5835932/install-and-configure-rpy2-on-opensuse?rq=1
+
+sudo apt-get install python-rpy2
