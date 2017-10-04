1403012623db511e92af3e6bcf32549c8b1c8e3e
Ahmed Bani <tazjel@gmail.com>
2017-10-04 07:46:57 +0300

    Create install_language-pack-ar.sh

---
+++ zten/zero/install/install_language-pack-ar.sh
@@ -0,0 +1,7 @@
+# Add Locale to Ubuntu Server 
+
+sudo apt-get install language-pack-ar
+
+# If language-pack-ar-base is not installed on your compter then the command 'dpkg -L language-pack-ar-base' will give followin error.
+
+dpkg -L language-pack-ar-base
