dc8f2c01dafc779b0eaedf32b9da71ebf4fed376
Ahmed Bani <tazjel@gmail.com>
2017-11-05 04:40:53 +0300

    Update install_space-vim.sh

--- zten/second/install_space-vim.sh
+++ zten/second/install_space-vim.sh
@@ -1,2 +1,13 @@
+# More infor :
+# https://arusahni.net/blog/2015/03/switching-to-neovim-part-1.html
 
 bash <(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)
+
+#
+#if has("unix")
+#  let s:uname = system("uname")
+#  let g:python_host_prog='/usr/bin/python'
+#  if s:uname == "Darwin\n"
+#    let g:python_host_prog='/usr/local/bin/python' # found via `which python`
+#  endif
+# endif 
