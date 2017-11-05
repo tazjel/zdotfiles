53cae06125236602184b193aa69af471e855be46
Ahmed Bani <tazjel@gmail.com>
2017-11-05 04:36:37 +0300

    Create install_python_neovim.sh

---
+++ zten/second/install_python_neovim.sh
@@ -0,0 +1,10 @@
+
+# Debian
+
+# Neovim is in Debian.
+
+sudo apt-get install neovim
+# Python (:python) support is also installable via the Debian package manager.
+
+sudo apt-get install python-neovim
+sudo apt-get install python3-neovim
