8a997af384adb9231cb785a7fa46c89f7a36e444
Ahmed Bani <tazjel@gmail.com>
2017-11-06 06:20:48 +0300

    Create nodejs.sh

---
+++ zten/zero/install/nodejs.sh
@@ -0,0 +1,3 @@
+# http://nodesource.com/blog/installing-node-js-tutorial-ubuntu/
+# Adding the NodeSource APT repository for Debian-based distributions repository AND the PGP key for verifying packages
+# Install Node.js from the Debian-based distributions repository
