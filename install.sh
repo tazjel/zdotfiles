47ab5e0640f9ab3edc0b7ff08c93a4addfb672e4
Ahmed Bani <tazjel@gmail.com>
2018-06-25 14:21:17 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+
+sudo apt-get update
+sudo apt-get install -y \
+  ranger \
+  zsh \
+  xclip \
+  xsel \
+  git-core \
+  curl \
+  wget \
+  autoconf \
+  automake \
+  cmake \
+  g++ \
+  libncurses5-dev \
+  libtool \
+  libtool-bin \
+  libunibilium-dev \
+  libunibilium0 \
+  pkg-config \
+  python-pip \
+  software-properties-common \
+  python3-dev \
+  python-dev \
+  unzip
