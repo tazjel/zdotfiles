df88893822cd3d6cc181e135d31fbd3c2c455048
Ahmed Bani <tazjel@gmail.com>
2018-08-20 17:44:58 +0300

    Create install_plasma-desktop.sh

---
+++ zten/zero/install/install_plasma-desktop.sh
@@ -0,0 +1,6 @@
+
+# Try it!"It neither installed full kde-desktop, nor installed any more annoying apps nor modified my bootup configuration. I'm now using KDE without any problems, and the best part is my PC does". From :
+# https://askubuntu.com/questions/999244/how-to-install-kde-plasma-without-installing-full-kubuntu-desktop
+
+
+sudo apt-get install plasma-desktop
